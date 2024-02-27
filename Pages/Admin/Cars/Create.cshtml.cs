97becd3d98f0f1f4b7379ae9d205ca74f087bd9e
mKarlsson14 <mackankb98@hotmail.se>
2024-02-27 13:52:42 +0100

    Full CRUD for car

--- Pages/Admin/Cars/Create.cshtml.cs
+++ Pages/Admin/Cars/Create.cshtml.cs
@@ -13,16 +13,22 @@ namespace FribergCarRentals_Foxtrot.Pages.Admin.Cars
     public class CreateModel : PageModel
     {   
         private readonly ICar carRep;
+        private readonly ICategory catRep;
 
-        public CreateModel(ICar carRep)
+        public SelectList Categories { get; set; }
+        public CreateModel(ICar carRep, ICategory catRep)
         {        
             this.carRep = carRep;
+            this.catRep = catRep;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            Categories = new SelectList(await catRep.GetAllCategoriesAsync(), nameof(Category.CategoryId), nameof(Category.Name));
+
             return Page();
         }
+       
 
         [BindProperty]
         public Car Car { get; set; } = default!;
@@ -30,10 +36,11 @@ namespace FribergCarRentals_Foxtrot.Pages.Admin.Cars
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+            Car.Category = await catRep.GetCategoryByIdAsync(Car.Category.CategoryId);
 
            await carRep.AddAsync(Car);            
 
